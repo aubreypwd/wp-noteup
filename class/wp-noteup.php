7241fad6f794adbddf0f9377150272878c25992e
Aubrey Portwood <aubreypwd@gmail.com>
2015-02-17 15:25:08 -0700

    * Don't hide the <body> * Docblocks for current functions

--- class/wp-noteup.php
+++ class/wp-noteup.php
@@ -8,35 +8,62 @@ class WP_NoteUp {
 
 	function __construct() {
 
-		// Get the file data.
-		$this->plugin_headers = get_file_data( dirname( __FILE__ ) .'/../wp-noteup.php', array(
-			'name'        => 'Plugin Name',
-			'plugin_uri'  => 'Plugin URI',
-			'version'     => 'Version',
-			'description' => 'Description',
-			'author'      => 'Author',
-			'author-uri'  => 'Author URI',
-			'text-domain' => 'Text Domain',
-			'domain-path' => 'Domain Path',
-		), 'plugin' );
-
 		// Set the name and version based on headers.
-		$this->text_domain = $this->plugin_headers['text-domain'];
-		$this->version = $this->plugin_headers['version'];
+		$this->text_domain = $this->get_plugin_info( 'Text Domain' );
+		$this->version = $this->get_plugin_info( 'Version' );
 
 		// Hooks
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 	}
 
+	/**
+	 * Set the plugin header info.
+	 *
+	 * @return void
+	 */
+	function set_plugin_info() {
+		$this->plugin_headers = get_file_data( dirname( __FILE__ ) .'/../wp-noteup.php', array(
+			'Plugin Name' => 'Plugin Name',
+			'Plugin URI' => 'Plugin URI',
+			'Version' => 'Version',
+			'Description' => 'Description',
+			'Author' => 'Author',
+			'Author URI' => 'Author URI',
+			'Text Domain' => 'Text Domain',
+			'Domain Path' => 'Domain Path',
+		), 'plugin' );
+	}
+
+	/**
+	 * Get a particular header value.
+	 *
+	 * @param  string $key The value of the plugin header.
+	 *
+	 * @return void
+	 */
+	function get_plugin_info( $key ) {
+		return $this->plugin_headers[ $key ];
+	}
+
 	function set_plugin_file( $file ) {
 		$this->plugin_file = $file;
 	}
 
+	/**
+	 * Enqueue all the scripts.
+	 *
+	 * @return void
+	 */
 	function enqueue_scripts() {
 		wp_enqueue_script( 'wp-noteup-js', plugins_url( 'js/wp-noteup.js', $this->plugin_file ), array( 'jquery' ), $this->plugin_data['version'], false );
 	}
 
+	/**
+	 * Enqueue all the styles.
+	 *
+	 * @return void
+	 */
 	function enqueue_styles() {
 		wp_enqueue_style( 'wp-noteup-css', plugins_url( 'css/wp-noteup.css', $this->plugin_file ), array(), $this->plugin_data['version'], 'all' );
 	}
--- wp-noteup.php
+++ wp-noteup.php
@@ -1,23 +1,29 @@
 <?php
 
 /*
-Plugin Name: WP NoteUp
-Plugin URI:
-Description:
-Author: Aubrey Portwood
-Version: 1.0-dev
-Author URI:
-Text Domain: wp-noteup
+	Plugin Name: WP NoteUp
+	Plugin URI:
+	Description:
+	Author: Aubrey Portwood
+	Version: 1.0-dev
+	Author URI:
+	Text Domain: wp-noteup
 */
 
+// Make sure we aren't colliding with another function (rare).
 if ( ! function_exists( 'wp_noteup_init' ) ) {
+
 	/**
 	 * Creates our WP_NoteUp instance.
+	 *
 	 * @return void
 	 */
 	function wp_noteup_init() {
+
+		// The classes
 		require_once( 'class/wp-noteup.php' );
 
+		// Instance
 		$wp_noteup = new WP_NoteUp();
 
 		// Make sure the plugin file is always this file.
