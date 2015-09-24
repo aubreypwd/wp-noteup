bd0268e8323c684b3a3c73c13b29d39b9abb9ee9
Aubrey Portwood <aubreypwd@gmail.com>
2015-09-23 21:53:54 -0700

    Change link to wp.org page.

--- wp-noteup.php
+++ wp-noteup.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: WP NoteUp
-Plugin URI: https://github.com/aubreypwd/wp-noteup
+Plugin URI: https://wordpress.org/plugins/wp-noteup/
 Description: WP NoteUp allows you to take simple notes when you're editing your Posts or Pages.
 Author: Aubrey Portwood
 Version: 1.0
