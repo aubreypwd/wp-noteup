2954362a86a3c9bfc65ad6d77448f93b0376d32e
Aubrey Portwood <aubreypwd@gmail.com>
2017-02-17 15:10:47 -0700

    Comment up the file so we know what's up.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,35 @@
 /* globals module */
+
 module.exports = function(grunt) {
 	'use strict';
 
 	grunt.initConfig( {
+
+		/**
+		 * Deploy to WordPress.org
+		 *
+		 *     grunt wp_deploy
+		 *
+		 * @type {Object} Settings for deploy npm module.
+		 */
 		wp_deploy: {
 			deploy: {
 				options: {
+
+					// https://wordpress.org/plugins/wp-noteup/
 					plugin_slug: 'wp-noteup',
 					svn_user:    'aubreypwd',
+
+					// Files with banners, icons, etc.
 					assets_dir:  'wp-org-assets'
 				}
 			}
 		}
 	} );
 
-	// Load tasks
+	// Load tasks.
 	grunt.loadNpmTasks( 'grunt-wp-deploy' );
 
-	// Register tasks
-	grunt.registerTask( 'default', [ 'wordpressdeploy' ] );
+	// Register tasks.
+	grunt.registerTask( 'default', [] );
 };
